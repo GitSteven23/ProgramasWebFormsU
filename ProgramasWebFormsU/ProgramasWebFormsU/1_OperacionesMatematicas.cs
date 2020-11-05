b2a9419a73bbd0235f7ee84b3e2f6e25230f67da
Layne Silva <patico_cm_15@hotmail.com>
2020-11-04 21:56:51 -0500

    diseños Layne

--- ProgramasWebFormsU/ProgramasWebFormsU/1_OperacionesMatematicas.cs
+++ ProgramasWebFormsU/ProgramasWebFormsU/1_OperacionesMatematicas.cs
@@ -16,5 +16,63 @@ namespace ProgramasWebFormsU
         {
             InitializeComponent();
         }
+        decimal num1, num2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal suma;
+
+            num1 = Convert.ToDecimal(textBox1.Text);
+            num2 = Convert.ToDecimal(textBox2.Text);
+
+            suma = num1 + num2;
+
+            textBoxResultado.Text = suma.ToString();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            decimal resta;
+
+            num1 = Convert.ToDecimal(textBox1.Text);
+            num2 = Convert.ToDecimal(textBox2.Text);
+
+            resta = num1 - num2;
+
+            textBoxResultado.Text = resta.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Multiplicacion_Click(object sender, EventArgs e)
+        {
+            decimal multiplicacion;
+
+            num1 = Convert.ToDecimal(textBox1.Text);
+            num2 = Convert.ToDecimal(textBox2.Text);
+
+            multiplicacion = num1 * num2;
+
+            textBoxResultado.Text = multiplicacion.ToString();
+        }
+
+        private void Division_Click(object sender, EventArgs e)
+        {
+            decimal division;
+
+            num1 = Convert.ToDecimal(textBox1.Text);
+            num2 = Convert.ToDecimal(textBox2.Text);
+
+            division = num1 / num2;
+
+            textBoxResultado.Text = division.ToString();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
