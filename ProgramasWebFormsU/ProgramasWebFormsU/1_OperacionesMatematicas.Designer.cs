fd7f56e973456afc6f4c06ab4b0bf5b3661f5164
Steven Chacón <steven2010_13@hotmail.com>
2020-11-24 23:30:41 -0500

    Commit final

--- ProgramasWebFormsU/ProgramasWebFormsU/1_OperacionesMatematicas.Designer.cs
+++ ProgramasWebFormsU/ProgramasWebFormsU/1_OperacionesMatematicas.Designer.cs
@@ -38,13 +38,18 @@
             this.Resultado = new System.Windows.Forms.Label();
             this.textBoxResultado = new System.Windows.Forms.TextBox();
             this.Division = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // SumaBoton
             // 
-            this.SumaBoton.Location = new System.Drawing.Point(473, 108);
+            this.SumaBoton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SumaBoton.Location = new System.Drawing.Point(589, 155);
+            this.SumaBoton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SumaBoton.Name = "SumaBoton";
-            this.SumaBoton.Size = new System.Drawing.Size(95, 23);
+            this.SumaBoton.Size = new System.Drawing.Size(127, 41);
             this.SumaBoton.TabIndex = 0;
             this.SumaBoton.Text = "Suma";
             this.SumaBoton.UseVisualStyleBackColor = true;
@@ -52,9 +57,11 @@
             // 
             // Resta
             // 
-            this.Resta.Location = new System.Drawing.Point(473, 153);
+            this.Resta.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Resta.Location = new System.Drawing.Point(589, 204);
+            this.Resta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Resta.Name = "Resta";
-            this.Resta.Size = new System.Drawing.Size(95, 23);
+            this.Resta.Size = new System.Drawing.Size(127, 39);
             this.Resta.TabIndex = 1;
             this.Resta.Text = "Resta";
             this.Resta.UseVisualStyleBackColor = true;
@@ -62,9 +69,11 @@
             // 
             // Multiplicacion
             // 
-            this.Multiplicacion.Location = new System.Drawing.Point(473, 203);
+            this.Multiplicacion.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Multiplicacion.Location = new System.Drawing.Point(589, 251);
+            this.Multiplicacion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Multiplicacion.Name = "Multiplicacion";
-            this.Multiplicacion.Size = new System.Drawing.Size(95, 23);
+            this.Multiplicacion.Size = new System.Drawing.Size(144, 39);
             this.Multiplicacion.TabIndex = 2;
             this.Multiplicacion.Text = "Multiplicación";
             this.Multiplicacion.UseVisualStyleBackColor = true;
@@ -73,67 +82,101 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(76, 129);
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 144);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.Size = new System.Drawing.Size(143, 24);
             this.label1.TabIndex = 4;
             this.label1.Text = "Primer Número ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(262, 129);
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(254, 144);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.Size = new System.Drawing.Size(155, 24);
             this.label2.TabIndex = 5;
             this.label2.Text = "Segundo Número";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(65, 153);
+            this.textBox1.Location = new System.Drawing.Point(83, 173);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 6;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(252, 153);
+            this.textBox2.Location = new System.Drawing.Point(257, 173);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 7;
             // 
             // Resultado
             // 
             this.Resultado.AutoSize = true;
-            this.Resultado.Location = new System.Drawing.Point(65, 244);
+            this.Resultado.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Resultado.Location = new System.Drawing.Point(80, 235);
+            this.Resultado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Resultado.Name = "Resultado";
-            this.Resultado.Size = new System.Drawing.Size(55, 13);
+            this.Resultado.Size = new System.Drawing.Size(94, 24);
             this.Resultado.TabIndex = 8;
             this.Resultado.Text = "Resultado";
             this.Resultado.Click += new System.EventHandler(this.label3_Click);
             // 
             // textBoxResultado
             // 
-            this.textBoxResultado.Location = new System.Drawing.Point(145, 241);
+            this.textBoxResultado.Location = new System.Drawing.Point(182, 238);
+            this.textBoxResultado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxResultado.Name = "textBoxResultado";
-            this.textBoxResultado.Size = new System.Drawing.Size(119, 20);
+            this.textBoxResultado.Size = new System.Drawing.Size(157, 22);
             this.textBoxResultado.TabIndex = 9;
             // 
             // Division
             // 
-            this.Division.Location = new System.Drawing.Point(473, 253);
+            this.Division.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Division.Location = new System.Drawing.Point(589, 298);
+            this.Division.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Division.Name = "Division";
-            this.Division.Size = new System.Drawing.Size(95, 23);
+            this.Division.Size = new System.Drawing.Size(144, 44);
             this.Division.TabIndex = 10;
             this.Division.Text = "División";
             this.Division.UseVisualStyleBackColor = true;
             this.Division.Click += new System.EventHandler(this.Division_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProgramasWebFormsU.Properties.Resources.ECCI_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(83, 336);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 31);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(652, 64);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Operaciones matemáticas";
+            // 
             // _1_OperacionesMatematicas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.Division);
             this.Controls.Add(this.textBoxResultado);
             this.Controls.Add(this.Resultado);
@@ -144,9 +187,10 @@
             this.Controls.Add(this.Multiplicacion);
             this.Controls.Add(this.Resta);
             this.Controls.Add(this.SumaBoton);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "_1_OperacionesMatematicas";
             this.Text = "Operaciones Matemáticas";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,5 +208,7 @@
         private System.Windows.Forms.Label Resultado;
         private System.Windows.Forms.TextBox textBoxResultado;
         private System.Windows.Forms.Button Division;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
--- ProgramasWebFormsU/ProgramasWebFormsU/2_FuncionesMatematicas.Designer.cs
+++ ProgramasWebFormsU/ProgramasWebFormsU/2_FuncionesMatematicas.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(_2_FuncionesMatematicas));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -38,111 +39,130 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(36, 143);
+            this.textBox1.Location = new System.Drawing.Point(49, 260);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.Size = new System.Drawing.Size(165, 22);
             this.textBox1.TabIndex = 0;
-           
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(258, 143);
+            this.textBox2.Location = new System.Drawing.Point(48, 335);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(129, 20);
+            this.textBox2.Size = new System.Drawing.Size(171, 22);
             this.textBox2.TabIndex = 1;
-         
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(255, 127);
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 297);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.Size = new System.Drawing.Size(217, 24);
             this.label1.TabIndex = 2;
             this.label1.Text = "Ingrese segundo numero";
-           
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(33, 127);
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 232);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.Size = new System.Drawing.Size(202, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "Ingrese primer numero";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(32, 53);
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 141);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(183, 13);
+            this.label3.Size = new System.Drawing.Size(320, 24);
             this.label3.TabIndex = 4;
             this.label3.Text = "Ingrese la funcion  que desea operar:";
-          
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(116, 285);
+            this.textBox3.Location = new System.Drawing.Point(500, 298);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(125, 20);
+            this.textBox3.Size = new System.Drawing.Size(165, 22);
             this.textBox3.TabIndex = 7;
-        
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(113, 259);
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(496, 260);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.Size = new System.Drawing.Size(148, 24);
             this.label4.TabIndex = 8;
             this.label4.Text = "Su resultado es :";
-        
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(116, 197);
+            this.button1.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(500, 168);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(187, 40);
+            this.button1.Size = new System.Drawing.Size(249, 49);
             this.button1.TabIndex = 9;
-            this.button1.Text = "Calcular";
+            this.button1.Text = "CALCULAR";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(144, 9);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(283, 25);
-            this.label5.TabIndex = 20;
-            this.label5.Text = "FUNCIONES MATEMATICAS";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-           
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(36, 87);
+            this.comboBox1.Location = new System.Drawing.Point(49, 183);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(206, 21);
+            this.comboBox1.Size = new System.Drawing.Size(273, 24);
             this.comboBox1.TabIndex = 21;
-          
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(681, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 59;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(37, 26);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(594, 64);
+            this.label5.TabIndex = 58;
+            this.label5.Text = "Funciones matemáticas";
             // 
             // _2_FuncionesMatematicas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(541, 366);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(1028, 400);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox3);
@@ -151,9 +171,10 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "_2_FuncionesMatematicas";
             this.Text = "Funciones Matemáticas";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -170,7 +191,8 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
     }
 }
