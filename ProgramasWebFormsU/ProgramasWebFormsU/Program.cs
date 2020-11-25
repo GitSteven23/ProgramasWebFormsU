30bdb0a7dbb3a04d3a92457cbb0ce137d3885db5
Jonathan Gomez <Jonathan Gomez@LAPTOP-G8AGCN46>
2020-11-24 22:16:08 -0500

    merge

--- ProgramasWebFormsU/ProgramasWebFormsU/Program.cs
+++ ProgramasWebFormsU/ProgramasWebFormsU/Program.cs
@@ -16,7 +16,7 @@ namespace ProgramasWebFormsU
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new _7_SumatoriaPositivoNegativo());
         }
     }
 }
